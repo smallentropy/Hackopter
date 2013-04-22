51980364775ebd4060fe92152f5f626235ee6c96
smallentropy <smallentropy@gmail.com>
2013-04-21 16:13:30 -0800

    - Initial project files commit

---
+++ Hackopter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hackopter
+//
+//  Created by Krzysztof Szczepaniak on 21/04/2013.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
